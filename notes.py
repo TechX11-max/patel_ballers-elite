64b100aaed076148e84956a2a4e4971969259241
TechX11-max <sahilsumoha@gmail.com>
2025-11-06 11:33:04 -0800

    Changes for collision implementation
    Switched up my progress idea in collision. Added a backboard collision based on the previous classes made. Used various sources such as chatgpt in order to format correctly and help with certain ideas before writing code.

--- notes.py
+++ notes.py
@@ -1,2 +1,3 @@
 #This is my file to note down ideas without deleting each line of code constantly
 #-
+#created a new class in backboard collision but still need to implement it in main.py( will be done next class)
--- sprites.py
+++ sprites.py
@@ -45,6 +45,10 @@ class Ball(pygame.sprite.Sprite):
             self.pos.x = WIDTH - self.r
             self.vel.x = -self.vel.x * REST
 
+
+
+
+
     def draw(self, surf): # ball drawing method
         pygame.draw.circle(surf, self.color, (int(self.pos.x), int(self.pos.y)), self.r) # main ball
         # seam details
@@ -54,3 +58,32 @@ class Ball(pygame.sprite.Sprite):
         pygame.draw.arc(surf, (220, 100, 20), # second arc
                         (self.pos.x - self.r, self.pos.y - self.r // 2, self.r * 2, self.r), # seam position
                         math.radians(210), math.radians(330), 2) # second arc
+        
+    def backboard_collision(self, backboard_rect):  #(will convert this code into a different class shortly(incase))
+                                                    #if backboard_rect.collidepoint(self.pos.x + self.r, self.pos.y):
+                                                    #self.pos.x=backboard_rect.left - self.r
+                                                    #self.vel.x= -self.vel.x * REST
+
+            
+        
+        nearest_x = max(backboard_rect.left, min(self.pos.x, backboard_rect.right)) # clamp x to backboard edges
+        nearest_y = max(backboard_rect.top, min(self.pos.y, backboard_rect.bottom)) # clamp y to backboard edges
+        nearest_point = pygame.Vector2(nearest_x, nearest_y)
+
+        # Vector from the nearest point to ball center
+        offset = self.pos - nearest_point
+        dist = offset.length()
+
+        # If ball overlaps the backboard rectangle
+        if dist < self.r and dist != 0:
+            n = offset.normalize()  # normal vector (direction away from wall)
+            overlap = self.r - dist
+            self.pos += n * overlap  # push ball out of backboard
+
+            # Reflect velocity along normal
+            v_dot = self.vel.dot(n)
+            if v_dot < 0:
+                self.vel -= (1 + REST) * v_dot * n
+
+
+
