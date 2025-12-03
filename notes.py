baaf4cd30f444c113688301e8175754f825f2d89
TechX11-max <sahilsumoha@gmail.com>
2025-12-03 11:59:37 -0800

    Brainstorming for better refinement
    Just brainstorming on paper and putting it into code. I haven't quite figured out these implementations due to reduced time periods of coding. Most likely done by friday

--- notes.py
+++ notes.py
@@ -1,3 +1,12 @@
 #This is my file to note down ideas without deleting each line of code constantly
 #-
 #created a new class in backboard collision but still need to implement it in main.py( will be done next class)
+
+
+# what to do next?
+
+# need to refine the backboard collision(brainstorming on docs)
+# implement scoring system( done in main.py but need to refine it more)
+# improve rim collision physics( done in main.py but can be improved more)
+# add sound effects and visual effects( will be done after core mechanics are solid)
+# polish user interface and game flow( will be done at the end)
